ae245496bf97c012ee03118a5c727c646a30e765
mdfaiazalam <faiazalam632019@gmail.com>
2022-09-14 20:32:41 +0530

    Update tut01.py
    I have print all the eigth octant

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,56 @@
-def octact_identification(mod=5000):
-###Code
+import pandas as pd 
+import numpy as np
+df=pd.read_csv('octant_input.csv')
 
 
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-mod=5000
-octact_identification(mod)
+A=df['Time'].to_numpy()
+B=df['U'].to_numpy()
+C=df['V'].to_numpy()
+D=df['W'].to_numpy()
+        
+m1=np.mean(A)     
+P=A-m1
+m2=np.mean(B)
+Q=B-m2
+m3=np.mean(C)
+R=C-m3
+m4=np.mean(D)
+S=D-m4
+def get_octant(pA,pB,pC,pD):
+    for point in df:
+        if P<0 & Q<0:
+            if R<0 & S<0:
+                print("first_octant")
+            if(R<0 & S>0):
+                print("second_octant")
+            if(R>0 & S<0):
+                print("third_octant")
+            if(R>0 & S>0):
+                print("tenth_octant")
+        if P<0 & Q>0:
+             if R<0 & S<0:
+                print("fourth_octant")
+                if(R<0 & S>0):
+                    print("ninth_octant")
+                    if(R>0 & S<0):
+                        print("eleventh_octant")
+                        if(R>0 & S>0):
+                           print("fifteenth_octant")
+        if P>0 & Q<0:
+            if R<0 & S<0:
+                print("fith_octant")
+                if R<0 & S>0:
+                    print("seventh_octant")
+                    if R>0 & S<0:
+                        print("eigth_octant")
+            if R>0 & S>0:
+                print("forteenth_octant")
+        if P>0 & Q>0:
+            if R<0 & S<0:
+                print("sixth_octant")
+                if R<0 & S>0:
+                    print("thirteenth_octant")
+                    if R>0 & S<0:
+                        print("twethth_octant")
+                        if R>0 & S>0:
+                            print("sixteenth_octant")
