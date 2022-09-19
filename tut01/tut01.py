245134c36084c58ffb84399da82f71e3bc070373
mdfaiazalam <faiazalam632019@gmail.com>
2022-09-19 10:00:38 +0530

    Update tut01.py
    till now we have find out all the average of U,V,W

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,10 +1,17 @@
 import csv
-filename = "octant_input.csv"
-fields = []
-row = []
-with open(filename, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    #fields = next(csvreader)
-    #for row in csvreader
-    #rows.append(row)
+#using csv library
+import pandas as pd
+#installing pandas library for manipulating all the data
 
+df=pd.read_csv("octant_input.csv",'r')
+#reading csv file
+mod = int(input("Enter mod - "))
+
+avgu=df['U'].mean()
+avgv=df['V'].mean()
+avgw=df['W'].mean()
+#finding average of all the U,V,W
+df.loc[0,'U Avg']=avgu
+df.loc[0,'V Avg']=avgv
+df.loc[0,'W Avg']=avgw
+    
